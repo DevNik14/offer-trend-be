dc741fb18083f8729b2b16b8e03bf94102db2f3e
DevNik14 <nykyurchiyski@gmail.com>
2025-05-10 10:26:22 +0300

    chore(deps): add express, playwright

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello, FFS!");
+import express from "express"
+import { chromium } from "playwright"
+
+const app = express();
+let greet: string | undefined = "";
+
+(async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto("https://playwright.dev/");
+
+  const heading: string[] = await page.locator(".hero__title").allTextContents();
+  if (heading.length > 0) {
+    greet = heading[0]
+  } else {
+    greet = "hello"
+  }
+  console.log(heading);
+
+
+  await context.close();
+  await browser.close();
+})();
+
+app.get("/", (req, res) => {
+  res.write(greet);
+  res.end()
+})
+
+app.listen(3000, () => {
+  console.log("listening");
+
+})
