765c88d56537c633879c2193764b75dd789d842c
DevNik14 <nykyurchiyski@gmail.com>
2025-05-22 15:12:25 +0300

    chore: cleaned up the index file, added more test.dummy logic

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import userController from "../controllers/user-controller.js";
+import homeController from "../controllers/home-controller.js"
+
 
 export default async function start() {
   const app = express();
 
-  app.get("/", (req, res) => {
-    res.write("Hello, Node! <3");
-    res.end()
-  })
+  // app.get("/", (req, res) => {
+  //   res.send("Hello, Node! <3")
+  // })
+
+  app.use("/", homeController)
+
+  app.use("/user", userController);
 
   app.listen(3000, () => console.log("listening"))
 }
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,8 @@
-import express from "express"
-import { chromium } from "playwright"
-
 import runDB from "./config/db.js";
 import start from "./config/express.js";
-
-const app = express();
 
 start();
 
 runDB()
   .then(() => console.log("connected"))
   .catch(console.log);
-
-let greet: string | undefined = "";
-
-(async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-
-  await page.goto("https://playwright.dev/");
-
-  const heading: string[] = await page.locator(".hero__title").allTextContents();
-  if (heading.length > 0) {
-    greet = heading[0]
-  } else {
-    greet = "hello"
-  }
-  console.log(heading);
-
-
-  await context.close();
-  await browser.close();
-})();
