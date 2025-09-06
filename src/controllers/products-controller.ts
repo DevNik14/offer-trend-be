d69e2939e386b2f4c5df3a1cad92752e57a9441b
devnik14 <nykyurchiyski@gmail.com>
2025-09-06 17:10:10 +0300

    improve: add 404 status code and error message handling for kaufland products

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -4,13 +4,13 @@ import scrape from "../scrapers/kaufland-scraper.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/kaufland", async (req, res) => {
   const data = await scrape()
 
   if (data.length > 0) {
     res.status(200).send(data);
   } else {
-    res.send("Something went wrong!");
+    res.status(404).send("No products at this times");
   }
 })
 
