674a41d2b4c87009a148fbe3ac1030f08b4528db
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2025-09-29 21:10:42 +0300

    refactor: migrate from MongoDB to SQLite and add test DB init config (#7)

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,23 +1,5 @@
-import User from "../models/User.js";
 import express from "express";
 
 const router = express.Router();
-
-router.get("/", async (req, res) => {
-  const user = await User.findOne({ name: "Pesho" });
-  res.send(user);
-})
-
-router.get("/:userId", async (req, res) => {
-  const { userId } = req.params;
-  console.log(userId);
-  const name = userId.split("").map((char, i) => (
-    i === 0 ? char.toUpperCase() : char
-  )).join("");
-  const user = await User.findOne({ name });
-  // res.send("User")
-  // res.send(user);
-  res.json(user)
-})
 
 export default router;
--- src/models/Product.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const productSchema = new Schema({
-  _id: String,
-  name: String,
-  description: String,
-  unitPrice: String,
-  basePrice: String
-});
