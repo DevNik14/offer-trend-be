7e9240f227b9b49a70bee4cc91a1fddf41763491
DevNik14 <nykyurchiyski@gmail.com>
2025-10-05 11:37:01 +0300

    feat: add better error handling for the scraping

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import db from "./config/db.js";
 import scrape from "./scrapers/kaufland-scraper.js";
 
 start();
+
 console.log(db);
 
+// scrape().then(data => console.log(data))
 
 // runDB()
 //   .then(() => console.log("connected"))
