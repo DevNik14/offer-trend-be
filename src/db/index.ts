2e8076f2ca612f1006f7dd636f3389a21ab93eb4
devnik14 <nykyurchiyski@gmail.com>
2025-12-04 16:04:39 +0200

    refactor: improve database initialization function

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,36 +1,41 @@
+import fs from "fs";
 import sqlite from "sqlite3";
 const sql3 = sqlite.verbose();
 
 import determineSlash from "../utils/determineSlash.js";
 
-const startDB = async () => {
+const dbPath = `${process.cwd()}${determineSlash}src${determineSlash}db${determineSlash}products.db`;
 
-  const db = new sql3.Database(`${process.cwd()}${determineSlash}src${determineSlash}db${determineSlash}products.db`, (err) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      db.run(
-        `CREATE TABLE IF NOT EXISTS products (
-          id TEXT PRIMARY KEY,
-          name TEXT,
-          description TEXT,
-          unitPrice TEXT,
-          basePrice TEXT,
-          image TEXT,
-          priceTagDiscount TEXT,
-          priceTag TEXT,
-          oldPriceTag TEXT
-        )`,
-        (err) => {
-          if (err) {
-            console.error(err.message);
-            return
-          }
-          console.log("Table created");
-        }
-      );
-    }
-  })
+const initSQLQUery = `CREATE TABLE IF NOT EXISTS products (
+  id TEXT PRIMARY KEY,
+  name TEXT,
+  description TEXT,
+  unitPrice TEXT,
+  basePrice TEXT,
+  image TEXT,
+  priceTagDiscount TEXT,
+  priceTag TEXT,
+  oldPriceTag TEXT
+)`;
+
+const createTable = (db: sqlite.Database) => db.run(initSQLQUery);
+
+const initDB = () => {
+
+  if (!fs.existsSync(dbPath)) {
+    const db = new sql3.Database(dbPath, (err) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      createTable(db);
+    })
+    console.log("Database created and connections established");
+    return db;
+  } else {
+    console.log("Connection established");
+    return new sql3.Database(dbPath);
+  }
+
 }
 
-export default startDB;
+export default initDB;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import start from "./config/express.js";
-import db from "./config/db.js";
 import scrape from "./scrapers/kaufland-scraper.js";
-import startDB from "./db/index.js";
+import initDB from "./db/index.js";
 
 start();
-startDB();
-
-// runDB()
-//   .then(() => console.log("connected"))
-//   .catch(console.log);
+initDB();
