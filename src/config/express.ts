13e48ab8ac777831b297f424208f4cd0ead13117
DevNik14 <nykyurchiyski@gmail.com>
2025-12-05 08:56:17 +0200

    feat: remove not needed routes

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,18 +1,12 @@
 import express from "express";
 import cors from "cors";
-import userController from "../controllers/user-controller.js";
-import homeController from "../controllers/home-controller.js"
 import productsController from "../controllers/products-controller.js";
 
 export default async function start() {
   const app = express();
   app.use(cors());
 
-  app.use("/", homeController)
-
-  app.use("/user", userController);
-
   app.use("/products", productsController);
 
-  app.listen(3000, () => console.log("listening"))
+  app.listen(3000, () => console.log("listening"));
 }
