dad200969fa1cbc67fb41b38444d36377aff33ae
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2026-01-05 22:20:00 +0200

    feat: remove sqlite3 and init db logic (#11)

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,4 @@
 import fs from "fs";
-import sqlite from "sqlite3";
-const sql3 = sqlite.verbose();
 
 import determineSlash from "../utils/determineSlash.js";
 
@@ -18,23 +16,10 @@ const initSQLQUery = `CREATE TABLE IF NOT EXISTS products (
   oldPriceTag TEXT
 )`;
 
-const createTable = (db: sqlite.Database) => db.run(initSQLQUery);
 
 const initDB = () => {
 
-  if (!fs.existsSync(dbPath)) {
-    const db = new sql3.Database(dbPath, (err) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      createTable(db);
-    })
-    console.log("Database created and connections established");
-    return db;
-  } else {
-    console.log("Connection established");
-    return new sql3.Database(dbPath);
-  }
+  console.log("Not implemented");
 
 }
 
